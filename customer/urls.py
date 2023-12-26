ded609771cc8c9a173c78e4ceb056cf2ff52bf63
kulvinderkakarkk <kkgnduplacements@gmail.com>
2023-12-26 17:08:01 +1100

    update and delete operations implemented

--- customer/urls.py
+++ customer/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('all/', views.allcustomers, name='customer.list'),
-    path('add/', views.AddCustomer, name='customer.add')
+    path('add/', views.AddCustomer, name='customer.add'),
+    path('delete/<int:pk>', views.DeleteCustomer, name='customer.delete'),
+    path('edit/<int:pk>', views.UpdateCustomer, name='customer.edit')
 ]
--- customer/views.py
+++ customer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CustomerRecords
 from django.contrib.auth.decorators import login_required
 from .models import customers
+from django.contrib import messages
 
 @login_required(login_url='home.login')
 def allcustomers(request):
@@ -17,5 +18,25 @@ def AddCustomer(request):
         old_customer_obj = form.save(commit=False)
         old_customer_obj.last_updated_by = request.user
         old_customer_obj.save()
+        messages.success(request, 'Customer '+old_customer_obj.customer_name+' created successfully!!')
         return redirect('customer.list')
-    return render(request, 'add_customer.html', {'form': form})
+    return render(request, 'add_customer.html', {'form': form, 'todo': 'add'})
+
+@login_required(login_url='home.login')
+def DeleteCustomer(request, pk):
+    selected_customer = customers.objects.get(id=pk)
+    selected_customer.delete()
+    messages.success(request, 'Customer '+selected_customer.customer_name+' deleted successfully!!')
+    return redirect('customer.list')
+
+@login_required(login_url='home.login')
+def UpdateCustomer(request, pk):
+    selected_customer = customers.objects.get(id=pk)
+    form = CustomerRecords(request.POST or None, instance=selected_customer)
+    if form.is_valid():
+        old_customer_obj = form.save(commit=False)
+        old_customer_obj.last_updated_by = request.user
+        old_customer_obj.save()
+        messages.success(request, 'Customer '+ selected_customer.customer_name +' updated sucessfully!!')
+        return redirect('customer.list')
+    return render(request, 'add_customer.html', {'form':form, 'todo': 'edit'})
